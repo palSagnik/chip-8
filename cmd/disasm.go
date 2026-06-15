0c8ee8e9665176a3c057b264871ccc7c7db3d57d
Sagnik Pal <sagnik.pal@grofers.com>
2026-06-16 01:36:46 +0530

    feat(disasm): recursive disassembly
    Signed-off-by: Sagnik Pal <sagnik.pal@grofers.com>

--- cmd/disasm.go
+++ cmd/disasm.go
@@ -28,26 +28,81 @@ type disasmObject struct {
 }
 
 func runDisasm(cmd *cobra.Command, args []string) error {
-
 	rom, err := os.ReadFile(disasmFile)
 	if err != nil {
 		log.Fatalf("could not read ROM: %v", err)
 	}
 
 	disasmOutput := make([]disasmObject, 0)
-	for i := 0; i < len(rom) - 1; i += 2 {
-		inst := uint16(rom[i]) << 8
-		inst |= uint16(rom[i + 1])
 
+	workList := []int{0}
+	visited := map[int]bool{}
+	for len(workList) > 0 {
+		address := workList[0]
+		workList = workList[1:]
+
+		// if visited then skip; else mark as true
+		if visited[address] {
+			continue
+		}
+		visited[address] = true
+		if address+1 >= len(rom) {
+			break
+		}
+
+		// decode inst
+		inst := uint16(rom[address]) << 8
+		inst |= uint16(rom[address + 1])
+		disasmInst := disassembleInstruction(inst)
 		disasmOutput = append(disasmOutput, disasmObject{
-			Memory: uint16(0x200 + i),
+			Memory: 0x200 + uint16(address),
 			Opcode: inst,
-			Instruction: disassembleInstruction(inst),
+			Instruction: disasmInst,
 		})
+
+
+		// pop the old address put the next one
+		memAddr := 0x200 + uint16(address)
+		nextAddress := findNextAddress(inst, memAddr)
+		// RET: no more address after here, but worklist might have address
+		if len(nextAddress) == 0 {
+			continue
+		}
+		for _, addr := range nextAddress {
+			workList = append(workList, int(addr - 0x200))
+		}
 	}
 
-	for i := range disasmOutput {
-		fmt.Printf("0x%X: 0x%X -> %s\n", disasmOutput[i].Memory, disasmOutput[i].Opcode, disasmOutput[i].Instruction)
+	for _, output := range disasmOutput {
+		fmt.Printf("0x%X: 0x%04X -> %s\n", output.Memory, output.Opcode, output.Instruction)
 	}
+
 	return nil
+}
+
+func findNextAddress(inst uint16, address uint16) []uint16 {
+	switch inst & 0xF000 {
+	case 0x0000:
+		if inst & 0xFF == 0xEE {
+			return []uint16{}
+		}
+	case 0x1000:
+		return []uint16{inst & 0xFFF}
+
+	// CALL has two branches, either it goes to the next after RET or jumps to addr
+	case 0x2000:
+		return []uint16{inst & 0xFFF, address + 2}
+	case 0x3000:
+		return []uint16{address + 2, address + 4}
+	case 0x4000:
+		return []uint16{address + 2, address + 4}
+	case 0x5000:
+		return []uint16{address + 2, address + 4}
+	case 0x9000:
+		return []uint16{address + 2, address + 4}
+	case 0xE000:
+		return []uint16{address + 2, address + 4}
+	}
+
+	return []uint16{address + 2}
 }
